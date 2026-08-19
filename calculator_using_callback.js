393238c7e9fc3325217f12d4a63efb46b873c4bb
Rashmi Rajput <rjay3261@gmail.com>
2026-08-19 10:07:34 +0530

    calculator

---
+++ calculator_using_callback.js
@@ -0,0 +1,17 @@
+function menu(a,b){
+    let add, sub, multiply, divide;
+    add=a+b;
+    sub=a-b;
+    multiply=a*b;
+    divide=a/b;
+    console.log(`Addition: ${add} Subtration: ${sub} multiplication: ${multiply} divition: ${divide}`)
+
+}
+// calculator(100,5);
+
+function calculator(callback){
+    callback(100,5);
+}
+calculator(menu);
+
+
