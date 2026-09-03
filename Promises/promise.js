ede36e3dfbfd63989c04acfaa3ade7bbdb0fdcac
Rashmi Rajput <rjay3261@gmail.com>
2026-09-03 11:17:51 +0530

    promise

--- Promises/promise.js
+++ Promises/promise.js
@@ -1,19 +1,43 @@
-//let promise = new Promise((resolve,reject)=>{
-    //console.log("I am new promise");
+// Structure of promise
+let promise = new Promise((resolve,reject)=>{
+    console.log("I am new promise");
+});
+resolve ("successfully executed"); // calling resolve execute code return data
+resolve(123);
+reject("(reject your request");  // calling this function will reject your request and genarate error msg
+});
 
 
-//resolve ("successfully executed"); // calling resolve execute code return data
 
-//reject("(reject your request");  // calling this function will reject your request and genarate error msg
-//});
-
-function GetData(dataId)
+function GetData(dataId) // ek function hai GetData
 {
     return new Promise((resolve,reject)=>{
-        setTimeout()=>{
+        setTimeout()=>{ //settimeout is aync function used to dealy
             console.log("DataId",dataId);
-            resolve("successful");
+            resolve("successful"); // sirf tabhi call hog jab data milega , nahi milega to error aa jayega
         },8000);
     });
 }
-let r=GetData(123);
+let r=GetData(123); //function id 123 jiska data humfetch kar rhe hain
+const GetPromise=()=>{
+    return new Promise((resolve,reject)=>{ // agar data milega resolve call hoga ,nahi milega to reject call hoga
+        console.log("i am a promise");
+        //resolve("successful");
+        reject("network error");
+    });
+};
+
+let promise=GetPromise();
+promise.then(()=>{
+    console.log("promise is fulfilled");
+});
+promise.catch(()=>{
+console.log("network is not working properly")
+});
+
+
+
+
+
+
+
